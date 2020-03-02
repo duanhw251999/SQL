c36afed4efc5f11567f6c2a1940dd14a79dcac47
duanhw251999 <47199465+duanhw251999@users.noreply.github.com>
2020-03-02 17:09:40 +0800

    Update 查看调度执行情况的脚本.sql

--- 查看调度执行情况的脚本.sql
+++ 查看调度执行情况的脚本.sql
@@ -2,5 +2,7 @@ SELECT  /*+parallel(32) */  * FROM ODSPMART.T_LOG_ETL_RUN A
 WHERE A.ETL_JOB='T_AWT_PRD_PROD_INST_A'
 ORDER BY A.EXEC_END_TIME DESC,A.PROCDURE_STEP DESC;
 
+Alter table ODSPMART.T_LOG_ETL_RUN parallel 1  ;
+
 
 --select  count(*) from othpdata.T_EVT_EVENT_TGPS a where a.load_date='2018-07-26'
